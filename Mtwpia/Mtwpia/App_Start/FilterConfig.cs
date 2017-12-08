073a61b6c3ba49869fa984fa1f72bf784e5f7a30
Corneleus <Corneleus@DESKTOP-L0FTV9P>
2017-12-07 19:18:52 -0500

    commit 1 created repo
    use templete for base mvc wb page, he says home
    
    updated references
    
    downloaded and installed resharper will help me keep in line with best practices and we might be geting it to use in the contract
    
    installed angular and angular route makes front end easier to deal with and is used in our contract
    
    installed log4net used in the contract and would allow for better logging in the application
    
    installed entity framework need this for database and would be a good thing to start with because we use it in the contract
    
    updated packages for the templete

---
+++ Mtwpia/Mtwpia/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using Mtwpia.Infrastructure;
+
+namespace Mtwpia.App_Start
+{
+    public class FilterConfig
+    {
+        public static void Configure(System.Web.Mvc.GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
