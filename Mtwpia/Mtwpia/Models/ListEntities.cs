160fabc503a2510c906c9fcd318e8d49db7fabad
Corneleus2 <Corneleus2@DESKTOP-C3LVRKR>
2017-12-20 09:26:58 -0500

    Got the Entity Model to work after forever it seems

---
+++ Mtwpia/Mtwpia/Models/ListEntities.cs
@@ -0,0 +1,20 @@
+//namespace Mtwpia.Models
+//{
+//    using System;
+//    using System.Data.Entity;
+//    using System.ComponentModel.DataAnnotations.Schema;
+//    using System.Linq;
+
+//    public partial class ListEntities : DbContext
+//    {
+//        public ListEntities()
+//            : base("name=ListEntities")
+//        {
+//        }
+
+
+//        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+//        {
+//        }
+//    }
+//}
